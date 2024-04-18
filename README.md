74694aeafcc9840bc206235c8a78091d50dc5590
Luu Thanh Sang <sangdung.ts@gmail.com>
2024-04-18 06:14:57 +0000

    beta release: 0.0.1-beta2404180

--- README.md
+++ README.md
@@ -4,3 +4,93 @@
 
 Easily build Eloquent queries from API requests using Alchemist Restful API.
 
+## Basic usage
+
+### Step 1: Define the API class
+
+```php
+class UserApiQuery extends AlchemistQueryable
+{
+    /**
+     * @param FieldSelector $fieldSelector
+     * @return void
+     */
+    public static function fieldSelector(FieldSelector $fieldSelector): void
+    {
+        $fieldSelector->defineFieldStructure([
+            'id', 'name', 'email'
+        ])
+        ->defineDefaultFields(['id']);
+    }
+
+    /**
+     * @param ResourceFilter $resourceFilter
+     * @return void
+     */
+    public static function resourceFilter(ResourceFilter $resourceFilter): void
+    {
+        $resourceFilter->defineFilteringRules([
+            FilteringRules::String('id', ['eq']),
+            FilteringRules::String('name', ['eq', 'contains']),
+            FilteringRules::String('email', ['eq', 'contains']),
+        ]);
+    }
+
+    /**
+     * @param ResourceOffsetPaginator $resourceOffsetPaginator
+     * @return void
+     */
+    public static function resourceOffsetPaginator(ResourceOffsetPaginator $resourceOffsetPaginator): void
+    {
+        $resourceOffsetPaginator->defineDefaultLimit(10)
+            ->defineMaxLimit(1000);
+    }
+
+    /**
+     * @param ResourceSearch $resourceSearch
+     * @return void
+     */
+    public static function resourceSearch(ResourceSearch $resourceSearch): void
+    {
+        $resourceSearch->defineSearchCondition('name');
+    }
+
+    /**
+     * @param ResourceSort $resourceSort
+     * @return void
+     * @throws AlchemistRestfulApiException
+     */
+    public static function resourceSort(ResourceSort $resourceSort): void
+    {
+        $resourceSort->defineDefaultSort('id')
+            ->defineDefaultDirection('desc')
+            ->defineSortableFields(['id', 'name']);
+    }
+}
+```
+### Step 2: Validate the input parameters
+
+Make sure to validate the input parameters passed in from the request input by using the `validate` method.
+
+```php
+
+// Assuming that the input parameters are passed in from the request input
+$input = [
+    'fields' => 'name,email',
+    'filtering' => [
+        'name:contains' => "John"
+    ],
+];
+
+$eloquentBuilder = EloquentBuilder::for(User::class, UserApiQuery::class, $input);
+
+if (! $eloquentBuilder->validate($e)->passes()) {
+    var_dump(json_encode($e->getErrors())); die();
+}
+```
+
+### Step 3: Get the result
+
+```php
+var_dump($eloquentBuilder->getBuilder()->toSql());
+```
---
+++ src/EloquentBuilder.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Nvmcommunity\EloquentApi;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Nvmcommunity\Alchemist\RestfulApi\AlchemistRestfulApi;
+use Nvmcommunity\Alchemist\RestfulApi\Common\Exceptions\AlchemistRestfulApiException;
+use Nvmcommunity\Alchemist\RestfulApi\Common\Integrations\AlchemistQueryable;
+use Nvmcommunity\Alchemist\RestfulApi\Common\Notification\ErrorBag;
+use Nvmcommunity\Alchemist\RestfulApi\FieldSelector\Handlers\FieldSelector;
+use Nvmcommunity\Alchemist\RestfulApi\ResourceFilter\Handlers\ResourceFilter;
+use Nvmcommunity\Alchemist\RestfulApi\ResourcePaginations\OffsetPaginator\Handlers\ResourceOffsetPaginator;
+use Nvmcommunity\Alchemist\RestfulApi\ResourceSearch\Handlers\ResourceSearch;
+use Nvmcommunity\Alchemist\RestfulApi\ResourceSort\Handlers\ResourceSort;
+
+class EloquentBuilder
+{
+    /**
+     * @param AlchemistRestfulApi $alchemistRestfulApi
+     * @param Builder $subject
+     */
+    public function __construct(protected AlchemistRestfulApi $alchemistRestfulApi, protected Builder $subject)
+    {
+        if ($alchemistRestfulApi->isComponentUses(FieldSelector::class)) {
+            $this->handleFieldSelector();
+        }
+
+        if ($alchemistRestfulApi->isComponentUses(ResourceFilter::class)) {
+            $this->handleResourceFilter();
+        }
+
+        if ($alchemistRestfulApi->isComponentUses(ResourceOffsetPaginator::class)) {
+            $this->handleOffsetPaginator();
+        }
+
+        if ($alchemistRestfulApi->isComponentUses(ResourceSort::class)) {
+            $this->handleResourceSort();
+        }
+
+        if ($alchemistRestfulApi->isComponentUses(ResourceSearch::class)) {
+            $this->handleResourceSearch();
+        }
+    }
+
+    /**
+     * @param Builder|Model|string $subject
+     * @param AlchemistQueryable|string $apiClass
+     * @param array $input
+     *
+     * @return EloquentBuilder
+     * @throws AlchemistRestfulApiException
+     */
+    public static function for(Builder|Model|string $subject, AlchemistQueryable|string $apiClass, array $input): EloquentBuilder
+    {
+        if (is_subclass_of($subject, Model::class)) {
+            $subject = $subject::query();
+        }
+
+        $alchemistRestfulApi = AlchemistRestfulApi::for($apiClass, $input);
+
+        return new static($alchemistRestfulApi, $subject);
+    }
+
+    /**
+     * @param ErrorBag|null $errorBag
+     * @return ErrorBag
+     */
+    public function validate(?ErrorBag &$errorBag = null): ErrorBag
+    {
+        return $this->alchemistRestfulApi->validate($errorBag);
+    }
+
+    /**
+     * @return AlchemistRestfulApi
+     */
+    public function getAlchemistRestfulApi(): AlchemistRestfulApi
+    {
+        return $this->alchemistRestfulApi;
+    }
+
+    /**
+     * @return Builder
+     */
+    public function getBuilder(): Builder
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @return void
+     */
+    protected function handleFieldSelector(): void
+    {
+        $rootNamespace = '$';
+
+        $fields = $this->alchemistRestfulApi->fieldSelector()->fields($rootNamespace);
+
+        foreach ($fields as $field) {
+            $fieldStructure = $this->alchemistRestfulApi->fieldSelector()->getFieldStructure("$rootNamespace.{$field->getName()}");
+
+            if ($fieldStructure['type'] === 'atomic') {
+                $this->subject->addSelect($field->getName());
+            }
+        }
+    }
+
+    /**
+     * @return void
+     */
+    protected function handleResourceFilter(): void
+    {
+        foreach ($this->alchemistRestfulApi->resourceFilter()->filtering() as $filteringObj) {
+            match ($filteringObj->getOperator()) {
+                'in' => $this->subject->whereIn($filteringObj->getFiltering(), $filteringObj->getFilteringValue()),
+                'not_in' => $this->subject->whereNotIn($filteringObj->getFiltering(), $filteringObj->getFilteringValue()),
+                'between' => $this->subject->whereBetween($filteringObj->getFiltering(), $filteringObj->getFilteringValue()),
+                'not_between' => $this->subject->whereNotBetween($filteringObj->getFiltering(), $filteringObj->getFilteringValue()),
+                'contains' => $this->subject->where(
+                    $filteringObj->getFiltering(), 'like', "%{$filteringObj->getFilteringValue()}%"
+                ),
+                default => $this->subject->where(
+                    $filteringObj->getFiltering(), $filteringObj->getOperator(), $filteringObj->getFilteringValue()
+                ),
+            };
+        }
+    }
+
+    /**
+     * @return void
+     */
+    protected function handleOffsetPaginator(): void
+    {
+        $offsetPaginate = $this->alchemistRestfulApi->resourceOffsetPaginator()->offsetPaginate();
+
+        if (! empty($offsetPaginate->getLimit())) {
+            $this->subject->limit($offsetPaginate->getLimit());
+        }
+
+        if (! empty($offsetPaginate->getOffset())) {
+            $this->subject->offset($offsetPaginate->getOffset());
+        }
+    }
+
+    /**
+     * @return void
+     */
+    protected function handleResourceSort(): void
+    {
+        $sort = $this->alchemistRestfulApi->resourceSort()->sort();
+
+        if (! empty($sort->getSortField())) {
+            $this->subject->orderBy($sort->getSortField(), $sort->getDirection());
+        }
+    }
+
+    /**
+     * @return void
+     */
+    protected function handleResourceSearch(): void
+    {
+        $search = $this->alchemistRestfulApi->resourceSearch()->search();
+
+        if (! empty($search->getSearchCondition())) {
+            $this->subject->where($search->getSearchCondition(), 'like', "%{$search->getSearchValue()}%");
+        }
+    }
+}
